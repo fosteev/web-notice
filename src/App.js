59ba3be7f4b2e5309b01d52c08fea021e4f589b0
Andrey <fosteev3@gmail.com>
2020-05-10 15:56:37 +0300

    Main screens

--- src/App.js
+++ src/App.js
@@ -1,21 +1,46 @@
-import React, {Component} from 'react'
+import React, {Component, Suspense} from 'react'
 import './App.css';
 import Button from '@material-ui/core/Button';
 import { Widget, addResponseMessage } from 'react-chat-widget';
 import 'react-chat-widget/lib/styles.css';
-
+import Login from "./screens/login";
+import Main from "./screens/main";
 import { subscribeToTimer, subscribeToChat } from './socket';
+import {
+    BrowserRouter,
+    Route,
+    Redirect
+} from 'react-router-dom';
+import Chats from "./screens/chats";
 
 const App = () => {
     subscribeToChat('5eaeaa3ba6bd332350f141e7', msg => {
         console.log(msg);
     })
 
-    return (
-       <div>
-           <Widget></Widget>
-       </div>
-    )
+    const login = localStorage.getItem('login');
+
+    if (login) {
+        return (
+            <BrowserRouter>
+                <div className='router'>
+                    <Suspense fallback={() => <p>Loading...</p>}>
+                        <Route path={'/'} component={Main}/>
+                    </Suspense>
+                </div>
+            </BrowserRouter>
+        )
+    } else {
+        return (
+            <BrowserRouter>
+                <div className='router'>
+                    <Suspense fallback={() => <p>Loading...</p>}>
+                        <Route path={'/'} component={Login}/>
+                    </Suspense>
+                </div>
+            </BrowserRouter>
+        )
+    }
 };
 
 export default App;
